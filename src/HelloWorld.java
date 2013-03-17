864136a4e3de45c47c9b4f0e01c9a943312861bc
Dev1 <vbv.rom@gmail.com>
2013-03-17 19:36:26 +0800

    Added username input in HelloWorld.java Added Person.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 class HelloWorld { 
  public static void main(String[] args) 
  { 
+ Scanner input = new Scanner(System.in);
+ System.out.println("Please enter your name:");
+ String name = input.next();
  System.out.println("Hello World!"); 
  } 
 } 
---
+++ src/Person.java
@@ -0,0 +1,9 @@
+class Person { 
+	String firstname;
+	String lastname;
+	
+	public Person(String fname, String lname){
+		firstname = fname;
+		lastname = lname;
+	}
+} 
